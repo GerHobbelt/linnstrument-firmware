9dfb8339ed48ec2c3f74d98b6b861def47750e28
Alex Brown <alexbbrown@gmail.com>
2016-01-08 03:00:27 -0800

    sensor reads now 16-bit.  this improves global performance by 5-10%
    # Conflicts:
    #	ls_sensor.ino

--- ls_sensor.ino
+++ ls_sensor.ino
@@ -258,12 +258,8 @@ inline unsigned short readZ() {                       // returns the raw Z value
 // spiAnalogRead:
 // returns raw ADC output at current cell
 inline short spiAnalogRead() {
-  byte msb = SPI.transfer(SPI_ADC, 0, SPI_CONTINUE);         // read byte MSB
-  byte lsb = SPI.transfer(SPI_ADC, 0);                       // read byte LSB
+  short raw = SPI.transfer16(SPI_ADC, 0);         // read 16-bit byte pair
 
-  // assemble the 2 transfered bytes into an int
-  short raw = short(msb) << 8;
-  raw |= lsb;
   // shift the 14-bit value from bits 16-2 to bits 14-0:
   //
   // as the ADS7883 datasheet says:
