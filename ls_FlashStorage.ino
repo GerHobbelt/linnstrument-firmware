7fcbc4d5ae4ea8a3e1865a3d8aa65c038f0063a1
Ger Hobbelt <ger@hobbelt.com>
2026-03-17 21:27:58 +0100

    tweak: added ls_FlashStorage.ino to easily and quickly 'include' the *local* DueFlashStorage library code as part of the 'sketch' in the Arduino IDE build environment.

---
+++ ls_FlashStorage.ino
@@ -0,0 +1,9 @@
+
+#include "ls_FlashStorage.h"
+
+// ----------------------------------------------------------------
+
+#include "./libraries/DueFlashStorage/src/DueFlashStorage.cpp"
+#include "./libraries/DueFlashStorage/src/efc.cpp"
+#include "./libraries/DueFlashStorage/src/flash_efc.cpp"
+
