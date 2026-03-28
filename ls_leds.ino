d7b578dba999d2146b999d6fbd69c52d0ca356fb
Ger Hobbelt <ger@hobbelt.com>
2026-03-28 10:41:37 +0100

    optimization: LEDs column index array: use a reference to the appropriate ROM-based index array instead of copying that one without ever having the need to actually *change* any of those column index values afterwards ==> small reduction in RAM costs.
    # Conflicts:
    #	ls_leds.ino

--- ls_leds.ino
+++ ls_leds.ino
@@ -45,9 +45,9 @@ These functions handle the low-level communication with LinnStrument's 208 RGB L
   redRow7   redRow6   redRow5   redRow4   redRow3   redRow2   redRow1   redRow0
 */
 
-byte COL_INDEX[MAXCOLS];
 static const byte COL_INDEX_200[MAXCOLS] = {0, 1, 6, 11, 16, 21, 2, 7, 12, 17, 22, 3, 8, 13, 18, 23, 4, 9, 14, 19, 24, 5, 10, 15, 20, 25};
 static const byte COL_INDEX_128[MAXCOLS] = {0, 1, 6, 11, 16, 2, 7, 12, 3, 8, 13, 4, 9, 14, 5, 10, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const byte* COL_INDEX = COL_INDEX_200;
 
 // array holding contents of display
 byte leds[2][LED_ARRAY_SIZE];
@@ -60,14 +60,10 @@ bool ledDisplayEnabled = true;
 
 void initializeLeds() {
   if (LINNMODEL == 200) {
-    for (byte i = 0; i < MAXCOLS; ++i) {
-      COL_INDEX[i] = COL_INDEX_200[i];
-    }
+    COL_INDEX = COL_INDEX_200;
   }
   else if (LINNMODEL == 128) {
-    for (byte i = 0; i < MAXCOLS; ++i) {
-      COL_INDEX[i] = COL_INDEX_128[i];
-    }
+    COL_INDEX = COL_INDEX_128;
   }
 }
 
